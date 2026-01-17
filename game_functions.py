13d360098b2904137b4d2739a55397de228338e1
rosie-e-l <rel215@exeter.ac.uk>
2026-01-17 01:14:53 +0000

    modified/added to pick_value function in game_3

--- game_functions.py
+++ game_functions.py
@@ -2,7 +2,8 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    x = random.choice(poss_values)   
+    x = random.choice(poss_values)
+    poss_values.remove(x)   
     return x
 
 # function to be used in game_2: Higher or Lower
